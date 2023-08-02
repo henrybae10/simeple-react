5526f06dc59bf75f3ae4780d5a9fd2080285e447
henrybae10 <henrybae10@gmail.com>
2023-08-02 17:42:45 +0900

    add deps to useEffect

--- react-begin/src/App.js
+++ react-begin/src/App.js
@@ -3,9 +3,19 @@ import styles from "./App.module.css";
 
 function App() {
 	// create-react-app을 사용하면서 import를 통해 useState 바로 써줘도 된다.
+	const [keyword, setKeyword] = useState("");
+	const onChange = (event) => setKeyword(event.target.value);
+
+	useEffect(() => {
+		if (keyword !== "") {
+			console.log("searching keyword: " + keyword);
+		}
+	}, [keyword]);
+
 	const [counter, setCounter] = useState(0);
 	const onClick = () => setCounter((prev) => prev + 1);
 	console.log("render");
+
 	const iRunOnlyOnce = () => {
 		console.log("render only once");
 	};
@@ -19,7 +29,12 @@ function App() {
 	*/
 	return (
 		<div>
-			<h1 className={styles.title}>Hello!</h1>
+			<input
+				value={keyword}
+				onChange={onChange}
+				type="text"
+				placeholder="search"></input>
+			<h1 className={styles.title}>{keyword}</h1>
 			<button onClick={onClick}>Count</button>
 			<p>{counter}</p>
 		</div>
