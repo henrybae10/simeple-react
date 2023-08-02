8af89cb847181653ebb06d12119b25d814568300
henrybae10 <henrybae10@gmail.com>
2023-08-02 15:43:59 +0900

    add component and css.

--- react-begin/src/index.js
+++ react-begin/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Button from "./Button";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<App />
+		<Button text={"test"} />
 	</React.StrictMode>
 );
