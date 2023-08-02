6d91e802cab6b3475dad2a3504120a72db504570
henrybae10 <henrybae10@gmail.com>
2023-08-02 16:48:40 +0900

    code for why we need react effect.

--- react-begin/src/App.js
+++ react-begin/src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
+	// create-react-app을 사용하면서 import를 통해 useState 바로 써줘도 된다.
+	const [counter, setCounter] = useState(0);
+	const onClick = () => setCounter((prev) => prev + 1);
+	console.log("render");
 	return (
 		<div>
 			<h1 className={styles.title}>Hello!</h1>
+			<button onClick={onClick}>Count</button>
+			<p>{counter}</p>
 		</div>
 	);
 }
