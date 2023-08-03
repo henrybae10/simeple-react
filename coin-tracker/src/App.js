a302fff96788ba8f28131ff9300f476576da6ba7
henrybae10 <henrybae10@gmail.com>
2023-08-04 00:35:16 +0900

    coin tracker

---
+++ coin-tracker/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+function App() {
+	const [loading, setLoading] = useState(true);
+	const [coins, setCoins] = useState([]);
+	useEffect(() => {
+		fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
+			response.json().then((json) => {
+				setCoins(json);
+				setLoading(false);
+			})
+		);
+	}, []);
+
+	return (
+		<div>
+			<h1>The Coins! (count: {coins.length})</h1>
+			{loading ? <strong>Loading...</strong> : null}
+			<select>
+				{coins.map((coin, index) => {
+					return (
+						<option key={index}>
+							{coin.name} ({coin.symbol})
+						</option>
+					);
+				})}
+			</select>
+			<ul>
+				{coins.map((coin, index) => {
+					return (
+						<li key={index}>
+							{coin.name} ({coin.symbol})
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
+
+export default App;
